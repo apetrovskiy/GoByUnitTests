16f28fa487cc7e2c616a8a765393baedeb55c866
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-02 18:40:26 +0300

    red (#21)

--- second_sample/sum.go
+++ second_sample/sum.go
@@ -13,3 +13,10 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 
 	return
 }
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
