fe7534ea8bf7f258d06f9de314b7c0854a43c1a0
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-02 16:45:17 +0300

    green (#9)

--- first_sample/hello.go
+++ first_sample/hello.go
@@ -8,6 +8,11 @@ func hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return helloPrefix + name
 }
 
