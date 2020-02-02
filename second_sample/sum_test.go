50e0116eafb413998d9d092be09b775082d585c5
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-02 18:04:22 +0300

    red and green

--- second_sample/sum_test.go
+++ second_sample/sum_test.go
@@ -3,7 +3,7 @@ package secondsample
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
 	want := 15
@@ -11,4 +11,15 @@ func TestSum(t *testing.T) {
 	if want != got {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
