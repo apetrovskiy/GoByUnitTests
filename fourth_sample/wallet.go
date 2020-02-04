4a8fc1c2e858dc797995c0ba09e9231dce2d4875
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-04 04:21:19 +0300

    refactor

--- fourth_sample/wallet.go
+++ fourth_sample/wallet.go
@@ -2,12 +2,14 @@ package fourthsample
 
 import "fmt"
 
-type Wallet struct{ balance int }
+type Bitcoin int
 
-func (w *Wallet) Deposit(amount int) {
+type Wallet struct{ balance Bitcoin }
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Println("address of balance in Deposit is", &w.balance)
 	w.balance += amount
 }
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
