5d37bfdfd36d3849a04eb678920511e1fd64e572
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-09 01:19:23 +0300

    cucumber (#62)
    * 1 (#1)
    
    * green (#2)
    
    * refactor (#3)
    
    * red
    
    * green
    
    * red (#4)
    
    * example 002 finished (#7)
    
    * red (#8)
    
    * green (#9)
    
    * refactor (#10)
    
    * green (#11)
    
    * refactor (#12)
    
    * refactoring 02 (#13)
    
    * red (#14)
    
    * green (#15)
    
    * refactor (#16)
    
    * red and green (#17)
    
    * red (#18)
    
    * green (#19)
    
    * refactor (#20)
    
    * red (#21)
    
    * green (#22)
    
    * red (#23)
    
    * green (#24)
    
    * refactor (#25)
    
    * refactor (#27)
    
    * red (#29)
    
    * green (#30)
    
    * red (#31)
    
    * green (#32)
    
    * red and green (#33)
    
    * red (#34)
    
    * green (almost) (#35)
    
    * green (#36)
    
    * Feature/third example 003 refactor (#37)
    
    * green
    
    * refactor
    
    * refactoring to test tables (#38)
    
    * red (#39)
    
    * green (#40)
    
    * refactor (#41)
    
    * red (#43)
    
    * green (#44)
    
    * refactor (#45)
    
    * refactor 02 (#46)
    
    * Feature/fourth example 002 red (#47)
    
    * red
    
    * red
    
    * green (#48)
    
    * refactor (#49)
    
    * red (#50)
    
    * green (#51)
    
    * refactor (#52)
    
    * red (#53)
    
    * green (#54)
    
    * Update go.yml
    
    * refactor (#55)
    
    * refactor 02 (#56)
    
    * red (#58)
    
    * Feature/fifth example 001 green (#59)
    
    * red
    
    * green
    
    * refactor (#60)
    
    * Feature/sixth example 001 red (#61)
    
    * original state
    
    * cucumber 001

---
+++ fourth_sample/wallet_service_test.go
@@ -0,0 +1,48 @@
+package fourthsample
+
+import (
+	"fmt"
+	"github.com/cucumber/godog"
+)
+
+var wallet Wallet
+
+func beforeScenario(interface{}) {
+	wallet = Wallet{}
+}
+
+func thereIsInWallet(amount int) error {
+	wallet.Deposit(Bitcoin(amount))
+	// fmt.Fprintf(os.Stdout, "actual balance (background) %s", wallet.Balance())
+	return nil
+}
+
+func iDeposit(amount int) error {
+	wallet.Deposit(Bitcoin(amount))
+	// fmt.Fprintf(os.Stdout, "actual balance (deposit) %s", wallet.Balance())
+	return nil
+}
+
+func iWithdraw(amount int) error {
+	wallet.Withdraw(Bitcoin(amount))
+	// fmt.Fprintf(os.Stdout, "actual balance (withdrawal) %s", wallet.Balance())
+	return nil
+}
+
+func amountIs(expectedTotalAmount int) error {
+	// fmt.Fprintf(os.Stdout, "expected %d", expectedTotalAmount)
+	// fmt.Fprintf(os.Stdout, "actual %s", wallet.Balance())
+	// fmt.Fprintf(os.Stdout, "%v", wallet.Balance() != Bitcoin(expectedTotalAmount))
+	if wallet.Balance() != Bitcoin(expectedTotalAmount) {
+		return fmt.Errorf("expected balance %s, actual balance %s", Bitcoin(expectedTotalAmount), wallet.Balance())
+	}
+	return nil
+}
+
+func FeatureContext(s *godog.Suite) {
+	s.BeforeScenario(beforeScenario)
+	s.Step(`^there is (\d+) in wallet$`, thereIsInWallet)
+	s.Step(`^I deposit (\d+)$`, iDeposit)
+	s.Step(`^I withdraw (\d+)$`, iWithdraw)
+	s.Step(`^amount is (\d+)$`, amountIs)
+}
